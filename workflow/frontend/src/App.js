e556389752fd0e8b1c75b5beb8c6012b64008b86
Konstantin Spasov <kspasov@lely.com>
2022-05-02 07:57:28 +0300

    .

--- workflow/frontend/src/App.js
+++ workflow/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          test 3!
         </p>
         <a
           className="App-link"
